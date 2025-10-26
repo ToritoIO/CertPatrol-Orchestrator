0b9bb9899054856119eac1156642a117bd94b7c7
Martín Aberastegue <martin@aberastegue.com.ar>
2025-10-26 20:06:55 +0100

    Update README image path and bump version to 0.1.2 in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setup(
     name="certpatrol-orchestrator",
-    version="0.1.1",
+    version="0.1.2",
     author="Martin Aberastegue",
     author_email="martin.aberastegue@torito.io",
     description="Process orchestration platform for managing multiple CertPatrol instances",
